65ae77d970d12c653c68b42362fa947ba89ba94e
HappiiDreamer <happiidreamer@gmail.com>
2023-04-01 09:56:58 -0500

    Creating a loan sends an email to each borrower.

--- src/apis/v1/loan.js
+++ src/apis/v1/loan.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Member, Loan, Activity } = require('../../models/all');
+const { Member, Loan, Activity, getMembersLoans } = require('../../models/all');
 const { secure } = require('../secure');
 
 router.post('/create', async (req, res) => {
@@ -44,6 +44,44 @@ router.post('/create', async (req, res) => {
         affectedLoans: [loan._id]
       });
       await activity.save();
+
+      // Send emails
+      for (let i = 0; i < req.body.borrowers.length; i++) {
+        const borrower = await Member.findById(req.body.borrowers[i]);
+
+        const emPrincipal = loan.records[0].amount.toLocaleString('en-US', { 
+          minimumFractionDigits: 2,
+          maximumFractionDigits: 2
+        });
+        const emInterest = (loan.interest * 100).toFixed(3);
+        const emGpe = loan.gracePeriodEnd.toLocaleDateString('en-US', {
+          month: 'short',
+          day: '2-digit',
+          year: 'numeric'
+        });
+        const emAccountTotal = (await getMembersLoans(borrower._id)).total.toLocaleString('en-US', { 
+          minimumFractionDigits: 2,
+          maximumFractionDigits: 2
+        });
+
+        await borrower.sendMail({
+          subject: 'New Loan Created!',
+          html: `
+          <h2>New Loan Created!</h2>
+          <p>A new loan was created on your lendr.cc account for <span style="color: #9029f4; font-weight: bold">\$${emPrincipal}</span></p>
+          <p>Information about your loan can be found below or at <a href="https://www.lendr.cc/loan/${loan._id}">https://www.lendr.cc/loan/${loan._id}</a></p>
+          <div style="font-family: monospace; margin-top: 2em;">
+            <p>ID: <strong>${loan._id}</strong></p>
+            <p>Memo: <strong>${loan.memo}</strong></p>
+            <p>Principal: <strong>\$${emPrincipal}</strong></p>
+            <p>Interest: <strong>${emInterest}% APR</strong></p>
+            <p>Grace Period End: <strong>${emGpe}</strong></p>
+            <br>
+            <p>Account Total: <strong>\$${emAccountTotal}</strong></p>
+          </div>
+          `
+        });
+      }
 
       res.send(loan);
     }
